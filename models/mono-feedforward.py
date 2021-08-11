3faca3e74ed52e68a87baaa0ab1679811644047f
MCooperBorkenhagen <cooperborken@wisc.edu>
2021-08-10 22:02:05 -0500

    runs for mono FF model, data saved locally

--- models/mono-feedforward.py
+++ models/mono-feedforward.py
@@ -6,6 +6,7 @@ from keras.layers import Input, Dense
 import numpy as np
 from utilities import reshape
 import pandas as pd
+import time
 
 # X and Y
 X = np.genfromtxt('data/mono-feedforward-train-orth.csv', delimiter=',')
@@ -16,7 +17,7 @@ Y_test = np.genfromtxt('data/mono-feedforward-test-phon.csv', delimiter=',')
 
 #
 testwords = pd.read_csv('data/mono-test.csv')
-
+trainwords = pd.read_csv('data/mono-train.csv')
 #%%
 
 orth = Input(shape=(X.shape[1], ), name='input')
@@ -28,19 +29,43 @@ model = Model(inputs=orth, outputs=phon, name='mono-feedforward')
 
 model.compile(optimizer='adam', loss="categorical_crossentropy", metrics=metric)
 model.summary()
-# %%
-items = open('../outputs/mono/feedforward/item-data.csv', 'w')
-items.write('epoch,word,accuracy,loss\n')
 
-CYCLES = 1
+# %% at four cycles, this takes about 8 minutes
+start = time.time()
+
+items = open('../outputs/mono/feedforward/item-data.csv', 'w')
+items.write('epoch,word,accuracy,loss,train_test\n')
+
+CYCLES = 4
 EPOCHS = 80
 for cycle in range(1, CYCLES+1):
-    model.fit(X, Y, batch_size=120, epochs=10, validation_split=0)
+    model.fit(X, Y, batch_size=120, epochs=EPOCHS, validation_split=0, sample_weight=np.array(trainwords['freq_scaled']))
+
+    yhat_train = np.empty(Y.shape)
+    yhat_test = np.empty(Y_test.shape)
+
+    for i, xy in enumerate(zip(X, Y)):
+        word = trainwords.iloc[i]['word']
+        epochs = cycle*EPOCHS
+        loss, acc = model.evaluate(xy[0].reshape((1, xy[0].shape[0])), xy[1].reshape((1, xy[1].shape[0])))
+        items.write('{},{},{},{},{}\n'.format(epochs, word, acc, loss, 'train'))
+        yhat = model.predict(xy[0].reshape((1, xy[0].shape[0])))
+        yhat_train[i] = yhat
 
     for i, xy in enumerate(zip(X_test, Y_test)):
         word = testwords.iloc[i]['word']
         epochs = cycle*EPOCHS
         loss, acc = model.evaluate(xy[0].reshape((1, xy[0].shape[0])), xy[1].reshape((1, xy[1].shape[0])))
-        items.write('{},{},{},{}\n'.format(epochs, word, acc, loss))
-# %%
+        items.write('{},{},{},{},{}\n'.format(epochs, word, acc, loss, 'test'))
+        yhat = model.predict(xy[0].reshape((1, xy[0].shape[0])))
+        yhat_test[i] = yhat
+
+    np.savetxt('../outputs/mono/feedforward/test-outputs-'+str(cycle)+'.csv', yhat_test)
+    np.savetxt('../outputs/mono/feedforward/train-outputs-'+str(cycle)+'.csv', yhat_train)
+
+end = time.time()
+
 items.close()
+"""Done"""
+"""Learntime was {} minutes""".format(round((end-start)/60))
+#%%
