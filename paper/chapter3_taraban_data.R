36bcd991157309c080ccd9db7a6444de728b798b
MCooperBorkenhagen <cooperborken@wisc.edu>
2021-09-22 23:28:26 -0500

    taraban redo

--- paper/chapter3_taraban_data.R
+++ paper/chapter3_taraban_data.R
@@ -2,15 +2,35 @@
 
 
 
+
+t9 = read_csv('../outputs/taraban/item-data-taraban-9.csv') %>% 
+  mutate(epoch = '9') %>% 
+  select(-cycle)
+
 t18 = read_csv('../outputs/taraban/item-data-taraban-18.csv') %>% 
   mutate(epoch = '18') %>% 
   select(-cycle)
+
+t27 = read_csv('../outputs/taraban/item-data-taraban-27.csv') %>% 
+  mutate(epoch = '27') %>% 
+  select(-cycle)
+
 t36 = read_csv('../outputs/taraban/item-data-taraban-36.csv') %>% 
   mutate(epoch = '36') %>% 
   select(-cycle)
+
+t45 = read_csv('../outputs/taraban/item-data-taraban-45.csv') %>% 
+  mutate(epoch = '45') %>% 
+  select(-cycle)
+
 t54 = read_csv('../outputs/taraban/item-data-taraban-54.csv') %>% 
   mutate(epoch = '54') %>% 
   select(-cycle)
+
+t63 = read_csv('../outputs/taraban/item-data-taraban-63.csv') %>% 
+  mutate(epoch = '63') %>% 
+  select(-cycle)
+
 t72 = read_csv('../outputs/taraban/item-data-taraban-72.csv') %>% 
   mutate(epoch = '72') %>% 
   select(-cycle)
@@ -50,14 +70,30 @@ jaredA_conditions = read_csv('../inputs/raw/jared_1997_appendixA.csv') %>%
 
 #jaredC_conditions = read_csv('../inputs/raw/jared_1997_appendixC.csv')
 
-frequency = read_csv('../outputs/taraban/frequency.csv', col_names = F) %>% 
-  rename(word = X1, freq = X2)  
+
+
+freq_train = read_csv('../models/data/taraban-train.csv') %>% 
+  mutate(train_test = 'train')
+
+
+
+freq_test = read_csv('../models/data/taraban-test.csv', col_names = 'word') %>% 
+  mutate(freq = NA,
+         freq_scaled = NA,
+         train_test = 'test')
+
+
+frequency = rbind(freq_train, freq_test)
 
 tarabanK = read_csv('../models/data/taraban-K.txt', col_names = F)[[1]]
 
-taraban = t18 %>% 
+taraban = t9 %>% 
+  full_join(t18) %>% 
+  full_join(t27) %>% 
   full_join(t36) %>% 
+  full_join(t45) %>% 
   full_join(t54) %>% 
+  full_join(t63) %>% 
   full_join(t72) %>% 
   left_join(syllabics) %>% 
   left_join(taraban_conditions) %>% 
@@ -68,8 +104,6 @@ taraban = t18 %>%
 taraban_testmode = read_csv('../outputs/taraban/taraban-generalization-epoch72.csv')
 
 
-
-taraban$freq_taraban = ordered(taraban$freq_taraban, c('low', 'high'))
 
 
 rm(t18, t36, t54, t72, frequency, syllabics, taraban_conditions, jaredA_conditions)
