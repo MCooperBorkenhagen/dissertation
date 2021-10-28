5ddfd576c3c0519eaaff1c8905281e05d96071ea
MCooperBorkenhagen <cooperborken@wisc.edu>
2021-10-27 22:23:06 -0500

    ...

--- paper/chateau_supplement.R
+++ paper/chateau_supplement.R
@@ -1,13 +1,71 @@
 
 # This script compiles the visuals for the two-by-three panel figure on the Chateau & Jared (2003) experiments
+COLORS = c('High' = 'grey86', 'Low' = 'black')
+# Experiment 2 (appendix A) ----
+# Model data
+N = multi %>% filter(epoch == 45) %>% filter(!is.na(chateauA_consistency)) %>% nrow()
 
 
 
+d = multi %>% 
+  filter(epoch == 45) %>% 
+  filter(!is.na(chateauA_consistency)) %>% 
+  mutate(consistency = case_when(chateauA_consistency == 'high' ~ 'High',
+                                 chateauA_consistency == 'low' ~ 'Low'))
 
 
-## Experiment 2 ----
+
+multi %>% 
+  filter(epoch == 45) %>% 
+  filter(!is.na(chateauA_consistency)) %>% 
+  group_by(chateauA_consistency) %>% 
+  summarise(SD = sd(mse),
+            mse = mean(mse),
+            SEM = SD/sqrt(N)) %>% 
+  mutate(consistency = case_when(chateauA_consistency == 'high' ~ 'High',
+                                 chateauA_consistency == 'low' ~ 'Low')) %>% 
+  ggplot(aes(consistency, mse)) +
+  geom_bar(stat = 'identity', position = position_dodge(), color = 'black') +
+  geom_errorbar(aes(ymin=mse-SEM, ymax=mse+SEM), position = position_dodge(.9), width = .1, color = 'grey23') +
+  geom_point(data = d, color = 'grey') +
+  scale_fill_manual(values = COLORS) +
+  labs(x = 'Consistency', y = 'Mean squared error', fill = 'Consistency') +
+  theme_apa()
+
+
+# human
+
+d = cj2003_means %>% 
+  filter(!is.na(chateauA_consistency)) %>% 
+  mutate(consistency = case_when(chateauA_consistency == 'high' ~ 'High',
+                                 chateauA_consistency == 'low' ~ 'Low'))
+
+
+
+cj2003_means %>% 
+  filter(!is.na(chateauA_consistency)) %>% 
+  group_by(chateauA_consistency) %>% 
+  summarise(SD = sd(rt),
+            rt = mean(rt),
+            SEM = SD/sqrt(N)) %>% 
+  mutate(consistency = case_when(chateauA_consistency == 'high' ~ 'High',
+                                 chateauA_consistency == 'low' ~ 'Low')) %>% 
+  ggplot(aes(consistency, rt)) +
+  geom_bar(stat = 'identity', position = position_dodge(), color = 'black') +
+  geom_errorbar(aes(ymin=rt-SEM, ymax=rt+SEM), position = position_dodge(.9), width = .1, color = 'grey23') +
+  geom_point(data = d, color = 'grey', position = position_jitter(.01)) +
+  scale_fill_manual(values = COLORS) +
+  labs(x = 'Consistency', y = 'RT (msec)', fill = 'Consistency') +
+  coord_cartesian(ylim = c(550, 700)) +
+  theme_apa()
+
+
+
+## Experiment 3 (appendix B) ----
 # Model data
 N = multi %>% filter(epoch == 45) %>% filter(!is.na(chateauB_consistency)) %>% nrow()
+
+COLORS = c('High' = 'grey86', 'Low' = 'black')
 
 d = multi %>% 
   filter(epoch == 45) %>% 
@@ -27,7 +85,6 @@ multi %>%
   lm(mse ~ consistency*frequency, data = .) %>% 
   summary()
 
-COLORS = c('High' = 'grey86', 'Low' = 'black')
 
 multi %>% 
   filter(epoch == 45) %>% 
@@ -47,3 +104,88 @@ multi %>%
   scale_fill_manual(values = COLORS) +
   labs(x = 'Frequency', y = 'Mean squared error', fill = 'Consistency') +
   theme_apa()
+
+
+# human data ----
+d = cj2003_means %>% 
+  filter(!is.na(chateauB_consistency)) %>% 
+  mutate(consistency = case_when(chateauB_consistency == 'high' ~ 'High',
+                                 chateauB_consistency == 'low' ~ 'Low'),
+         frequency = case_when(chateauB_frequency == 'high' ~ 'High',
+                               chateauB_frequency == 'low' ~ 'Low'))
+
+cj2003_means %>% 
+  filter(!is.na(chateauB_consistency)) %>% 
+  group_by(chateauB_consistency, chateauB_frequency) %>% 
+  summarise(SD = sd(rt),
+            rt = mean(rt),
+            SEM = SD/sqrt(N)) %>% 
+  mutate(consistency = case_when(chateauB_consistency == 'high' ~ 'High',
+                                 chateauB_consistency == 'low' ~ 'Low'),
+         frequency = case_when(chateauB_frequency == 'high' ~ 'High',
+                               chateauB_frequency == 'low' ~ 'Low')) %>% 
+  ggplot(aes(frequency, rt, fill = consistency)) +
+  geom_bar(stat = 'identity', position = position_dodge(), color = 'black') +
+  geom_errorbar(aes(ymin=rt-SEM, ymax=rt+SEM), position = position_dodge(.9), width = .1, color = 'grey23') +
+  geom_point(data = d, position = position_jitterdodge(dodge.width = .9, jitter.width = .05, jitter.height = .0005), color = 'grey') +
+  scale_fill_manual(values = COLORS) +
+  labs(x = 'Frequency', y = 'RT (msec)', fill = 'Consistency') +
+  coord_cartesian(ylim = c(550, 700)) +
+  theme_apa()
+
+
+# Experiment 4 (appendix C) ----
+# model data ----
+N = multi %>% filter(epoch == 45) %>% filter(!is.na(chateauC_consistency)) %>% nrow()
+
+COLORS = c('High' = 'grey86', 'Low' = 'black')
+
+d = multi %>% 
+  filter(epoch == 45) %>% 
+  filter(!is.na(chateauC_consistency)) %>% 
+  mutate(consistency = case_when(chateauC_consistency == 'high' ~ 'High',
+                                 chateauC_consistency == 'low' ~ 'Low'))
+
+
+multi %>% 
+  filter(epoch == 45) %>% 
+  filter(!is.na(chateauC_consistency)) %>% 
+  group_by(chateauC_consistency) %>% 
+  summarise(SD = sd(mse),
+            mse = mean(mse),
+            SEM = SD/sqrt(N)) %>% 
+  mutate(consistency = case_when(chateauC_consistency == 'high' ~ 'High',
+                                 chateauC_consistency == 'low' ~ 'Low')) %>% 
+  ggplot(aes(consistency, mse)) +
+  geom_bar(stat = 'identity', color = 'black') +
+  geom_errorbar(aes(ymin=mse-SEM, ymax=mse+SEM), position = position_dodge(.9), width = .1, color = 'grey23') +
+  geom_point(data = d, color = 'grey', position = position_jitter(.02)) +
+  scale_fill_manual(values = COLORS) +
+  labs(x = 'Frequency', y = 'Mean squared error', fill = 'Consistency') +
+  theme_apa()
+
+# human data ----
+d = cj2003_means %>% 
+  filter(!is.na(chateauC_consistency)) %>% 
+  mutate(consistency = case_when(chateauC_consistency == 'high' ~ 'High',
+                                 chateauC_consistency == 'low' ~ 'Low'))
+
+
+cj2003_means %>% 
+  filter(!is.na(chateauC_consistency)) %>% 
+  group_by(chateauC_consistency) %>% 
+  summarise(SD = sd(rt),
+            rt = mean(rt),
+            SEM = SD/sqrt(N)) %>% 
+  mutate(consistency = case_when(chateauC_consistency == 'high' ~ 'High',
+                                 chateauC_consistency == 'low' ~ 'Low')) %>% 
+  ggplot(aes(consistency, rt)) +
+  geom_bar(stat = 'identity', color = 'black') +
+  geom_errorbar(aes(ymin=rt-SEM, ymax=rt+SEM), position = position_dodge(.9), width = .1, color = 'grey23') +
+  geom_point(data = d, color = 'grey', position = position_jitter(.02)) +
+  scale_fill_manual(values = COLORS) +
+  labs(x = 'Frequency', y = 'RT (msec)', fill = 'Consistency') +
+  coord_cartesian(ylim = c(550, 700)) +
+  theme_apa()
+
+
