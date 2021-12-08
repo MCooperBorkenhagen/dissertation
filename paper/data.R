bf503165cb5f4ca0ddf2eab6cd2a38b18954bbbd
MCooperBorkenhagen <cooperborken@wisc.edu>
2021-12-08 15:49:11 -0600

    paper, more reorg

---
+++ paper/data.R
@@ -0,0 +1,5 @@
+
+
+source('data/data_monosyllabic.R')
+source('data/data_monosyllabic_crossval.R')
+source('data/data_multisyllabic.R')
--- paper/load.R
+++
@@ -1,9 +0,0 @@
-
-
-options(scipen = 100)
-
-
-aoa = readxl::read_xlsx('../inputs/raw/AoA_ratings_Kuperman_et_al_BRM.xlsx')
-wcbc = read.csv('~/Documents/words/american_child_texts/data/clean/corpustable.csv')
-wcbc_metadata = read_xlsx('~/Documents/words/american_child_texts/data/raw/book/metadata.xlsx') %>% 
-  select(doc_id = isbn, age = earliest_advert_level_months)
