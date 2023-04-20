d3f1dfe27187bf47299fd52a08f4450d351d7e4f
MCooperBorkenhagen <cooperborken@wisc.edu>
2023-04-20 17:00:21 -0500

    catching up on deletes and modifications to paper/

--- models/tmp.py
+++ models/tmp.py
@@ -1,18 +1,50 @@
+
+
+
+# lets just use the simple (mono) corpus for this, to accelerate training time
+
 #%%
-from utilities import load, get_words, scale
+import LearnerRS
 import numpy as np
-train = load('data/chateau/train.traindata')
+from utilities import load, loadreps
 
-trainfreqs = {word: freq for k, v in train.items() for word, freq in zip(v['wordlist'], v['frequency'])}
+test = load('data/mono-lstm-test.traindata')
+train = load('data/mono-lstm-train.traindata')
 
+
+# phonreps and orthreps
+phonreps = loadreps('../inputs/phonreps-with-terminals.json')
+orthreps = loadreps('../inputs/raw/orthreps.json')
+# get frequencies for all words:
+frequencies = {word: v['frequency'][i] for v in train.values() for i, word in enumerate(v['wordlist'])}
+# add the test frequencies
+for k, v in test.items():
+    for i, word in enumerate(v['wordlist']):
+        frequencies[word] = v['frequency'][i]
+
+# here k is equal to the phonological length of the word + 1 (because of the terminal segment)
+#assert test.keys() == train.keys(), 'Phonological lengths represented in test and train are not the same. Resample'
+
+orth_features = train[4]['orth'].shape[2]
+phon_features = train[4]['phonSOS'].shape[2]
+# probabilities for sampling phonological lengths during fitcycle() for training
+probs = [.3, .4, .2, .075, .025]
+assert len(probs) == len(train.keys()), 'Pick probabilities that match the phonological lengths in train.traindata'
+#
+learner = Learner.Learner(orth_features, phon_features, phonreps=phonreps, orthreps=orthreps, traindata=train, testdata=test, loss='binary_crossentropy', modelname='tmp', hidden=400, devices=False)
+train_frequencies = {word: v['frequency'][i] for k, v in train.items() for i, word in enumerate(v['wordlist'])}
 p = .93
-maxf = max(trainfreqs.values())
+maxf = max(train_frequencies.values())
 K = p/np.log(maxf)
+
+
 #%%
-with open('data/chateau/train.csv', 'w') as f:
-    f.write('word,freq,freq_scaled\n')
-    for word in get_words(train, verbose=False):
-        freq = trainfreqs[word]
-        freq_scaled = scale(freq, K)
-        f.write('{},{},{}\n'.format(word, freq, freq_scaled))
-f.close()
+
+# 0 condition: null phon on epoch 0
+C = 1
+CYCLES = 1
+for i in range(1, C+1):
+    cycle = i*CYCLES
+    cycle_id = str(cycle)    
+    learner.fitcycle(batch_size=32, null_phon=False, cycles=CYCLES, probs=probs, K=K, evaluate=False, cycle_id=cycle_id, outpath='../outputs/tmp/') 
+
--- paper/tmp.R
+++ paper/tmp.R
@@ -1,13 +1,25 @@
-#\includepdf[pages=-]{5_appendix1.pdf}
 
 
-V = 'ZH'
 
-multi_testmode %>% 
-  filter(str_detect(phon, V)) %>% 
-  select(word, phon)  
+\begin{rotate}{180}
+\includegraphics{graphic.pdf}
+\end{rotate}
 
-multi_testmode %>% 
-  filter(str_detect(phon, V)) %>% 
-  select(word, phon)  %>% 
-  View()
+
+\pagenumbering{gobble}
+
+\newcommand{\Lpagenumber}{\ifdim\textwidth=\linewidth\else\bgroup
+\dimendef\margin=0
+\ifodd\value{page}\margin=\oddsidemargin
+\else\margin=\evensidemargin
+\fi
+\raisebox{\dimexpr -\topmargin-\headheight-\headsep-0.03\linewidth}[0pt][0pt]{
+  \rlap{\hspace{\dimexpr \margin+\textheight+\footskip}
+    \llap{\rotatebox{90}{\thepage}}}}
+\egroup\fi}
+\AddEverypageHook{\Lpagenumber}
+
+
+\begin{landscape}
+\pagestyle{empty}
+\newpage
